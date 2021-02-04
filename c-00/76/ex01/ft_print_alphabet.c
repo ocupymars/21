0d91396ecf0d6f07bff90747f1f4d15f54b0f365
ocupymars <78388199+ocupymars@users.noreply.github.com>
2021-02-04 23:47:08 +0300

    Add files via upload

---
+++ c-00/76/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 14:05:03 by wlaureen          #+#    #+#             */
+/*   Updated: 2021/02/04 21:53:00 by wlaureen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
---
+++ c-00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 15:12:16 by wlaureen          #+#    #+#             */
+/*   Updated: 2021/02/04 22:00:16 by wlaureen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+}
