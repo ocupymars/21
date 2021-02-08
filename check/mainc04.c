9deff2d725b4b201f4de2d249789e3f718ac41c8
ocupymars <78388199+ocupymars@users.noreply.github.com>
2021-02-08 23:41:43 +0300

    Add files via upload

---
+++ check/mainc04.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainc04.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 23:27:44 by wlaureen          #+#    #+#             */
+/*   Updated: 2021/02/08 23:40:11 by wlaureen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+//ex00
+int		main(void)
+{
+	char	l;
+
+	l = "luciddreams";
+	printf("%i\n", l);
+}
+
+/*ex01
+int		main(void)
+{
+	char	*str;
+
+	str = "beachesaintloyal";
+	printf("%s\n", str);
+}*/
