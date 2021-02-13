611bba0f8e35ef793f033fb891443855aa4f2f47
ocupymars <78388199+ocupymars@users.noreply.github.com>
2021-02-13 23:23:16 +0300

    Add files via upload

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 22:25:51 by wlaureen          #+#    #+#             */
+/*   Updated: 2021/02/13 22:37:51 by wlaureen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum {
+	false = 0,
+	true = 1
+}	t_bool;
+
+# define FALSE false
+# define TRUE true
+
+# define EVEN(number) (number % 2 == 0)
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define SUCCESS 0
+
+#endif
